94f1a37a4625c249ed8d503de4ea27a697e60543
Marifel <mrbarbasa@gmail.com>
2014-12-03 20:00:40 -1000

    Load About page contents when About link is clicked

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,11 +1,19 @@
 $(function() {
+  function loadContentArea() {
+    $.get( "/api/about.json", function( data ) {
+      $(".content_text .row .columns").html(data.body);
+    }, "json" );
+  }
+
+  loadContentArea();
+
   $("#specs").click(function() {
     $.get( "/api/spec.json", function( data ) {
       $(".content_text .row .columns").html(data.body);
     }, "json" );
   });
 
-  $.get( "/api/about.json", function( data ) {
-    $(".content_text .row .columns").html(data.body);
-  }, "json" );
+  $("#about").click(function() {
+    loadContentArea();
+  });
 });
